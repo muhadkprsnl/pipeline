b5131a9c1aa8c7d7a95b9bd91c38aee22a66ac55
muhadk <muhadk123@gmail.com>
2025-02-20 15:44:14 +0530

    resolve conflic2t

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -5,6 +5,9 @@ dotenv.config();
 
 export default defineConfig({
   e2e: {
+    video: true,  // Enables video recording
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     baseUrl: process.env.BASE_URL || "https://academy-dev.dartle.app/",
     setupNodeEvents(on, config) {
       // Implement node event listeners here
